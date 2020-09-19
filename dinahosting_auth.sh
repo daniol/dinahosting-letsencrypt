3f342aec7b738da9b3b96f521b4cd9a6afcb737b
daniol <dani88elx@gmail.com>
2020-09-19 23:15:02 +0200

    Fix exitcode

--- dinahosting_auth.sh
+++ dinahosting_auth.sh
@@ -6,7 +6,9 @@ source dinahosting_vars.sh
 # Create TXT record
 responseCode=$( curl -s "https://dinahosting.com/special/api.php?AUTH_USER=$API_USER&AUTH_PWD=$API_PASS&command=Domain_Zone_AddTypeTXT&domain=$CERTBOT_DOMAIN&hostname=_acme-challenge&text=$CERTBOT_VALIDATION&responseType=json&SIMULATE=false" | jq -r '.responseCode' )
 echo "Creating TXT-Entry to $CERTBOT_DOMAIN: $responseCode"
-[[ "responseCode" = "1000" ]] || exit 1
+[[ "$responseCode" = "1000" ]] || exit 1
 
 # Sleep to make sure the change has time to propagate over to DNS (Dinahosting TXT TTL: 5 min)
 sleep 6m
+
+exit 0
--- dinahosting_cleanup.sh
+++ dinahosting_cleanup.sh
@@ -5,11 +5,11 @@ source dinahosting_vars.sh
 
 # Remove the challenge TXT record(s)
 if [ -n "${CERTBOT_DOMAIN}" ]; then
-
-while : ; do
- responseCode=$( curl -s "https://dinahosting.com/special/api.php?AUTH_USER=$API_USER&AUTH_PWD=$API_PASS&command=Domain_Zone_DeleteTypeTXT&domain=$CERTBOT_DOMAIN&hostname=_acme-challenge&responseType=json&SIMULATE=false" | jq -r '.responseCode' )
- echo "Cleaning TXT-Entry: $responseCode"
- [[ "responseCode" = "1000" ]] || break
-done
-
+	while : ; do
+	 responseCode=$( curl -s "https://dinahosting.com/special/api.php?AUTH_USER=$API_USER&AUTH_PWD=$API_PASS&command=Domain_Zone_DeleteTypeTXT&domain=$CERTBOT_DOMAIN&hostname=_acme-challenge&responseType=json&SIMULATE=false" | jq -r '.responseCode' )
+	 echo "Cleaning TXT-Entry: $responseCode"
+	 [[ "$responseCode" = "1000" ]] || break
+	done
 fi
+
+exit 0
